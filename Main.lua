c13a651fb9a2a85ab52723211f372b94ac943e38
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-06-09 19:47:51 +1000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,38 +2,48 @@ if game.Players.LocalPlayer.PlayerGui:FindFirstChild("ScreenGui") then
     game.Players.LocalPlayer.PlayerGui:FindFirstChild("ScreenGui"):Destroy()
 end
 
+local function FrameLocalScript()
+    local script = Instance.new('LocalScript', Frame)
+
+    script.Parent.Active = true
+    script.Parent.Draggable = true
+end
+coroutine.wrap(FrameLocalScript)()
+
 local Library = {}
 
 function Library:CreateMain()
     local ScreenGui = Instance.new("ScreenGui")
-    ScreenGui.Name = "ScreenGui"
-    ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
-    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.Name = "ScreenGui"
+ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-    local Frame = Instance.new("Frame")
-    Frame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-    Frame.Position = UDim2.new(0.0892547667, 0, 0.188524336, 0)
-    Frame.Size = UDim2.new(0, 556, 0, 439)
-    Frame.Parent = ScreenGui
+local Frame = Instance.new("Frame")
+Frame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Frame.Position = UDim2.new(0.0892547667, 0, 0.188524336, 0)
+Frame.Size = UDim2.new(0, 556, 0, 439)
+Frame.Parent = ScreenGui
 
-    local UICorner = Instance.new("UICorner")
-    UICorner.Parent = Frame
+local UICorner = Instance.new("UICorner")
+UICorner.Parent = Frame
 
-    local Tabs = Instance.new("Frame")
-    Tabs.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-    Tabs.Position = UDim2.new(0.820143878, 0, 0, 0)
-    Tabs.Size = UDim2.new(0, 100, 0, 438)
-    Tabs.Parent = Frame
+local Tabs = Instance.new("Frame")
+Tabs.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Tabs.Position = UDim2.new(0.820143878, 0, 0, 0)
+Tabs.Size = UDim2.new(0, 100, 0, 438)
+Tabs.Parent = Frame
 
-    local UICorner_2 = Instance.new("UICorner")
-    UICorner_2.Parent = Tabs
+local UICorner_2 = Instance.new("UICorner")
+UICorner_2.Parent = Tabs
 
-    local UIListLayout = Instance.new("UIListLayout")
-    UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-    UIListLayout.Padding = UDim.new(0, 4)
-    UIListLayout.Parent = Tabs
+local UIListLayout = Instance.new("UIListLayout")
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIListLayout.Padding = UDim.new(0, 4)
+UIListLayout.Parent = Tabs
 
-    local GamerLibrary = {}
+game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame.Active = true
+Frame.Draggable = true
+
 
     function GamerLibrary:NewButton(name, callback)
         local textbutton = Instance.new("TextButton")
@@ -52,21 +62,6 @@ function Library:CreateMain()
         end)
 
         return textbutton
-    end
-
-    function GamerLibrary:AddTab(Name)
-        local button = Instance.new("TextButton")
-        button.Parent = Tabs
-        button.BackgroundColor3 = Color3.new(1, 1, 1)
-        button.Font = Enum.Font.GothamBold
-        button.Size = UDim2.new(1, 0, 0, 50)
-        button.Text = Name
-        button.TextColor3 = Color3.new(0, 0, 0)
-        button.FontSize = 12
-
-        -- Add your tab-specific logic here
-
-        return button
     end
 
     return GamerLibrary
