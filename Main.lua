9c2c4c4e8ccc2593b8ca0f27fc96a78b7579e601
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-06-09 18:58:07 +1000

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,52 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local UIListLayout = Instance.new("UIListLayout")
+
+local Library = {}
+
+--Properties:
+function Library:CreateMain()
+	ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+	Frame.Parent = ScreenGui
+	Frame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+	Frame.Position = UDim2.new(0.177642971, 0, 0.305450797, 0)
+	Frame.Size = UDim2.new(0, 556, 0, 439)
+
+	UICorner.Parent = Frame
+
+	UIListLayout.Parent = Frame
+	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+	UIListLayout.Padding = UDim.new(0, 4)
+	
+	local function RQPF_fake_script() -- Frame.LocalScript 
+		local script = Instance.new('LocalScript', Frame)
+
+		script.Parent.Active = true
+		script.Parent.Draggable = true
+	end
+	coroutine.wrap(RQPF_fake_script)()
+	
+	local GamerLibrary = {}
+	
+	 function GamerLibrary:NewButton(name)
+		local textbutton = Instance.new("TextButton")
+		textbutton.Parent = Frame
+		textbutton.BackgroundColor3 = Color3.new(0,0,0)
+		textbutton.Font = Enum.Font.GothamBold
+		textbutton.Text  = name
+		textbutton.TextColor3 = Color3.new(1,1,1)
+		textbutton.FontSize = 14
+		
+	end
+	return GamerLibrary
+end
+
+return Library;
